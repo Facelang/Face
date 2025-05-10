31dcd5de60a26a9c05d9bfcae6bf51e7bbe90a92
marshal.mac <mac@ikik.io>
2025-05-10 10:22:33 +0800

    -

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "face-lang/compiler/provider/asm"
 
 func main() {
+	//buf, err := os.ReadFile("common.t")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, b := range buf {
+	//	fmt.Printf("%d, ", b)
+	//}
 	_ = asm.Program("example/ass/common.s")
 	println("完成编译！")
 }
